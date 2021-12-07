1b26e4b8a21c43b8f6b7be4225def25fd987afa2
plms <askdell@sina.com>
2021-12-07 11:19:53 +0800

    Date类功能补充

--- J-programmes/DateTest/DateTest003.java
+++ J-programmes/DateTest/DateTest003.java
@@ -10,11 +10,15 @@ public class DateTest003 {
 		long millis=System.currentTimeMillis();
 		System.out.println(millis);
 
-		Date date=new Date(millis);
-		System.out.println("date==>" + date);
+		//使用毫秒数计算今天的日期
+		Date today=new Date(millis);
+		//计算昨天的日期
+		Date yesterday= new Date(System.currentTimeMillis() - 1000*60*60*24);
+		System.out.println("today==>" + today);
+		System.out.println("yesterday==>" + yesterday);
 
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss:SSS");
-		String nowDate = sdf.format(date);
+		String nowDate = sdf.format(today);
 		System.out.println("nowDate==>" + nowDate);
 
 		String str = sdf.format(System.currentTimeMillis());
@@ -23,7 +27,7 @@ public class DateTest003 {
 		
 		//把毫秒数转化为时分秒
 		SimpleDateFormat sdf2=new SimpleDateFormat("HH:mm:ss");
-		sdf2.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
+		sdf2.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));     //设置0时区，因为计算时间增量是以0时区为原点的，如果是GMT+08:00则结果加8小时
 		long mm=140200;
 		String hms=sdf2.format(mm);
 		System.out.println("hms==>" + hms);
