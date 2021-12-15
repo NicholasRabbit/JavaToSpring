c75ff16aa9e18711f26b2481e51454e7b3bb9b5c
plms <askdell@sina.com>
2021-12-15 22:41:51 +0800

    补充模拟Tomcat工作原理代码

---
+++ Servlet-programmes/A000-A模拟Servlet本质/个人模拟Tomcat代码/Servlet.java
@@ -0,0 +1,7 @@
+package com.javax.servlet;
+
+//模拟写一个Servlet接口
+public interface Servlet {
+
+	public abstract void service();  //先只写个service()方法
+}
---
+++ Servlet-programmes/A000-A模拟Servlet本质/个人模拟Tomcat代码/TomcatMy.java
@@ -0,0 +1,56 @@
+package com.javax.tomcat;
+
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+import com.javax.controller.LoginController;
+
+
+//模拟Tomcat服务器
+public class TomcatMy {
+
+	public static void main(String[] args){
+	
+		System.out.println("Tomcat服务器启动......");
+		System.out.println("开始接收浏览器请求.....");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("请输入路径：");
+		String  requestPath = scan.next();
+		FileReader reader = null;
+		Properties pro = null;
+		try{
+			reader = new FileReader("web.xml");  //读取配置文件
+			pro = new Properties();        
+			pro.load(reader);		//把配置文件加载到Properties对象里
+			
+			//用户在客户端输入/login,  对应的 value是LoginController
+			String className = pro.getProperty(requestPath);
+			System.out.println("className==>" + className);
+			//利用反射机制，通过用户输入的请求来找对应的处理类
+			Class controllerCalss = Class.forName("com.javax.controller." + className);
+			//实例化反射的对象
+			LoginController  controller = (LoginController)controllerCalss.newInstance();
+			controller.service();  //调用LoginController对象的方法service(),即进行处理浏览器端的请求
+
+		}catch(IOException e){
+			e.printStackTrace();
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}catch(InstantiationException e){
+			e.printStackTrace();
+		}catch(IllegalAccessException e){
+			e.printStackTrace();
+		}finally{
+			if(reader != null){
+				try{
+					reader.close();  //关闭IO流
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+}
