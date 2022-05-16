99f56952219d3f6cc9089397d8c11c624ca6783c
plms <askdell@sina.com>
2022-05-16 22:57:02 +0800

    @RequestHeader, @RequestBody  ,@RequestParam的使用区分

--- SSM/SpringMVCTest006/src/com/springframe/mvcJson/SpringMvcJson.java
+++ SSM/SpringMVCTest006/src/com/springframe/mvcJson/SpringMvcJson.java
@@ -14,7 +14,7 @@ import com.springframe.mvcJson.dao.UserDao;
 /*SpringMVC框架处理json的四个条件
  *(1)导入框架所需处理json依赖的jackson...jar包
  *(2)在SpringMVC框架本身的xml配置文件中配置标签<mvc:annotation-driven>,即告诉框架要识别并处理json了
- *(3)在处理请求的方法上加上@Responsebody
+ *(3)在处理请求的方法上加上@Responsebody，底层用的是jackson的jar包进行的转换
  *(4)把需要转换为json的数据或对象作为方法的返回值返回
  *
  * */
