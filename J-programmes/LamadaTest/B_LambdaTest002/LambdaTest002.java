b70135a7438b186fff1f3ac2bd02e8ebcf34899d
plms <askdell@sina.com>
2022-07-25 21:56:25 +0800

    add lambda002

---
+++ J-programmes/LamadaTest/B_LambdaTest002/LambdaTest002.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+/*
+根据Lamda课程，个人练习范例，初识Lambda。
+Lambda的常见用途
+*/
+public class LambdaTest002 {
+
+	public static void main(String[] args){
+		
+		anonymousTest();
+
+	}
+
+
+	//1，用在匿名内部类的创建上
+	public static void anonymousTest(){
+		//(1),原来的创建匿名内部类的方式
+		Comparator<String> comparator = new Comparator<String>(){
+			public int compare(String o1,String o2){
+				return Integer.compare(o1.length(),o2.length());
+			}
+		};
+
+		TreeSet<String> set01 = new TreeSet<String>(comparator);  //复习，放进Set集合内的对象要继承Comparable或写一个比较器Comparator
+		set01.add("ab");
+		set01.add("hijkl");
+		for(String s : set01){
+			System.out.println("set01==>" + s);
+		}
+		//(2),使用Lambda表达式的方式创建匿名内部类
+		Comparator<String> comparator02 = (x,y) -> Integer.compare(x.length(),y.length());
+		TreeSet<String> set02 = new TreeSet<>(comparator02);
+		set02.add("123");
+		set02.add("123456");
+		for(String s : set02){
+			System.out.println("set02==>" + s);
+		}
+
+	}
+}
