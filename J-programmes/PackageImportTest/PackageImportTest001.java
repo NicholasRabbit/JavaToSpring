18dc6ed86af7977957d8280ade63c64c770a6cc0
plms <askdell@sina.com>
2022-10-05 20:42:33 +0800

    增加javac编辑命令笔记

--- J-programmes/PackageImportTest/PackageImportTest001.java
+++ J-programmes/PackageImportTest/PackageImportTest001.java
@@ -1,3 +1,15 @@
+
+/*
+Java编译不在当前目录下的源码命令：
+--project
+	--src/com/test/Test.java
+	--out/com/test/Test.class (编译后的结果)
+project/--> : javac -encoding utf-8 out /src/com/test/Test.java :编译Test.java到out目录下
+运行：
+out/--> : java com.test.Test
+*/
+
+
 package com.cctv.sports.programmes;   //注意后面不要忘加分号
 
 public class PackageImportTest001 {
