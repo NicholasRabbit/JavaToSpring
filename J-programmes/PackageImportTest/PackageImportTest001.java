9d6a442261d0234dea8cd103c13feb7ba2d80352
plms <askdell@sina.com>
2022-10-06 15:02:32 +0800

    增加笔记

--- J-programmes/PackageImportTest/PackageImportTest001.java
+++ J-programmes/PackageImportTest/PackageImportTest001.java
@@ -6,11 +6,13 @@ Java编译不在当前目录下的源码命令：
 	--src/com/test/Test.java
 	--out/com/test/Test.class (编译后的结果)
 project/--> : javac -encoding utf-8 out /src/com/test/Test.java :编译Test.java到out目录下
-注意如果Test.java中引入了别的包要一起编译，否则编译报错，例:FactoryTest.java中引入了order和entity报下的类，所以要一起编译，此例为设计模式中的工厂模式中的范例
+注意:如果Test.java中引入了别的包要一起编译，否则编译报错，例:FactoryTest.java中引入了order和entity报下的类，所以要一起编译，此例为设计模式中的工厂模式中的范例
 javac -encoding utf-8 -d out src/com/factory/test/FactoryTest.java 
 							 src/com/factory/order/*.java  
 							 src/com/factory/entity/*.java
-
+另外，同一个包下的类，互相使用到了的话，也要一起编译，不能只编译一个，会报错
+例：com/prototype/no/Sheep.java,SheepTest.java
+SheepTest.java用到了Sheep.java,编译时也要同时编译
 
 (2)运行：
 out/--> : java com.test.Test
