eeba5372fac2dc82e4651842c4e7dcd94246111f
plms <askdell@sina.com>
2023-08-24 17:04:39 +0800

    MyBatis:if-foreach

---
+++ SSM/MyBatisTest005/src/com/mybatis/dynamic/bean/ProductPlanOutputMain.java
@@ -0,0 +1,17 @@
+package com.mybatis.dynamic.bean;
+
+
+import java.util.List;
+
+public class ProductPlanOutputMain {
+
+	private String type;
+
+
+	private List<Long> compIdList; //集合是对象的一个属性，作为foreach的的条件查询
+
+	private List<Emp> empList;
+
+
+
+}
