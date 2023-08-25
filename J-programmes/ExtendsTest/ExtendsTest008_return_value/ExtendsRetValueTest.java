acd5aba8b808d49dc53f4705a405bf53e031388d
plms <askdell@sina.com>
2023-08-25 14:16:56 +0800

    update code

---
+++ J-programmes/ExtendsTest/ExtendsTest008_return_value/ExtendsRetValueTest.java
@@ -0,0 +1,49 @@
+
+
+/**
+ * 继承时，子类重写的方法的返回值范围只能更小，不能更大，
+ * 即返回值可以是父类方法中返回值的子类，不能是其父类。
+ *
+ * 跟重写方法抛出异常一样的规则。
+ * */
+
+public class ExtendsRetValueTest {
+
+	public static void main(String[] args){
+		
+		new B();
+	
+	}
+
+}
+
+
+class Vehicle {
+
+
+}
+
+class Car extends Vehicle {
+
+}
+
+class Honda extends Car{
+
+}
+
+
+class A {
+	public Car run(){
+		System.out.println("vehicle run");
+		return null;
+	}
+}
+
+class B extends A {
+
+	//子类这里写覆盖父类的同名方法，返回值不能是父类中方法返回值的父类，可以是子类
+	public Vehicle run(){
+		System.out.println("car run");
+		return null;
+	}
+}
