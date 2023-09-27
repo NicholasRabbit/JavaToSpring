330b504ee5124dc1fe02a48e01a90c72bd08e610
plms <askdell@sina.com>
2023-09-27 16:59:24 +0800

    review break for1 or for2

--- J-programmes/BreakTest/BreakTest001.java
+++ J-programmes/BreakTest/BreakTest001.java
@@ -1,9 +1,14 @@
+
+/*
+ * break打断指定的循环
+ * 在多层循环下，给各个循环起别名后，可用break指定打断哪个循环
+ * */
 public class BreakTest001{
+
     public static void main(String[] args){
+
 	    for(int b=0;b<=10;b++){
-		
 		    if(b<6){
-			
 			    break;
 			}
 		
@@ -13,26 +18,37 @@ public class BreakTest001{
 		
         System.out.println("分隔符>>>>>>>>>>>>>>>>>>>>>>");
 
+		//注意break后循环名称在不同情况下的用法
         for1:for(int c=0;c<10;c++){
-		
+				 System.out.print(c + ":");
 		         for2:for(int d=0;d<5;d++){      
-				 
-				     if(c==4){                     //注意break后循环名称在不同情况下的用法
-					 
-					    break for2;
+				     if(c==4){                    
+					    break for2;  //这里打断for2代表的循环
 						//System.out.print("check");  //break终端for循环以后，后边的程序就不只执行了，此处报错
 					 }
-				     System.out.println(d);
-					 
-				 
+				     System.out.print(" " + d);
 				 }
-				 
+
+				System.out.println();
 		
 		}
-	
-	
-	
-	}
 
+		
+        System.out.println("分隔符>>>>>>>>>>>>>>>>>>>>>>");
+
+	    for1:for(int c=0;c<10;c++){
+				 System.out.print(c + ":");
+				 for2:for(int d=0;d<5;d++){      
+					 if(c==4){                     
+						break for1;  //这里打断外部的for1循环
+					 }
+					 System.out.print(" " + d);
+				 }
+
+				System.out.println();
+		
+		}
+
+	}
 
 }
