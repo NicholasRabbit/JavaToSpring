fec1cab831b23dfa9e2282cb468944d4eb7e938d
plms <wuyangzerg@gmail.com>
2023-11-07 10:30:52 +0800

    rename StreamSort.java

--- J-programmes/Stream Demo/StreamSortComparatorTest/StreamSortComparatorTest.java
+++ J-programmes/Stream Demo/StreamSortComparatorTest/StreamSortComparatorTest.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StreamComparatorTest {
+public class StreamSortComparatorTest {
 
     public static void main(String[] args) {
        sort();
