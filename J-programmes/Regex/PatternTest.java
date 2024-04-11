d623a2f4a74348ac9704368ecbfa8e72b15eb670
plms <wuyangzerg@gmail.com>
2024-04-11 14:27:06 +0800

    message

--- J-programmes/Regex/PatternTest.java
+++ J-programmes/Regex/PatternTest.java
@@ -11,8 +11,23 @@ public class PatternTest {
         Pattern pattern = Pattern.compile("\\$\\{.+\\}");
         //To check if the given input matches the pattern which represents the regular expression.
         Matcher matcher = pattern.matcher("${foo}");
+
+        /**
+         * Attention: The find() is to find the next subsequence. If matches() were called before it, then the find() would to match the next subsequence.
+         * Namely the find2 will be false after the 2.matches() was called.
+         * */
+        //1, find()
+        boolean find = matcher.find();
+        System.out.println("find: " + find);
+
+        //2, matches()
         boolean doesMatch = matcher.matches();
         System.out.println("doesMatch:" + doesMatch);
+
+
+        boolean find2 = matcher.find();
+        System.out.println("find: " + find2);  //false
+
 
     }
 }
