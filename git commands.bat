658029c3b7cedc67424715402b71cdc265549c29
plms <wuyangzerg@gmail.com>
2024-05-13 15:08:54 +0800

    extends with public, protected and so on

--- git commands.bat
+++ git commands.bat
@@ -4,13 +4,14 @@ git status
 
 git add --all
 
+: : Just directly write the committing message without any quoations marks.
 set /p message=Enter message for Command 1: 
 
-git commit -m message
+git commit -m "%message%"
+
+git push origin master
 
 git push origin-github master
 
-git push origin  master
-
-
 PAUSE
+
