dcce68822af96ed8f20f27b22d0b1f7e74265a9a
plms <wuyangzerg@gmail.com>
2024-06-14 15:31:58 +0800

    reflection: add constructor.newInstance

--- J-programmes/ReflectTest/ReflectTest005-Constructors/ConstructorReflectionTest.java
+++ J-programmes/ReflectTest/ReflectTest005-Constructors/ConstructorReflectionTest.java
@@ -1,6 +1,7 @@
 package com.tdd.practice;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 
 public class ConstructorReflectionTest {
@@ -15,7 +16,7 @@ public class ConstructorReflectionTest {
         // 1, How many constructors does User have?
         Constructor<?>[] constructors = userClass.getConstructors();
         System.out.println("The number of constructors is " + constructors.length);
-        // 2, The order of constructors.
+        // 2, The order of constructors is as same as the order in the source.
         for (Constructor c : constructors) {
             System.out.println(c.getParameters().length);
         }
@@ -23,6 +24,19 @@ public class ConstructorReflectionTest {
         for (Constructor c : constructors) {
             Parameter[] parameters = c.getParameters();
             System.out.println(parameters);
+        }
+
+        // 4, newInstance
+        Constructor<?> c = constructors[2];  // The third constructor.
+        try {
+            User tom = (User)c.newInstance(1, "Tom");
+            System.out.println(tom);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
 
 
