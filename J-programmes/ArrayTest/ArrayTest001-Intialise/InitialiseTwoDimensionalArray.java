93e479d042066715f56ce83fa4dc704f8efc92ca
plms <wuyangzerg@gmail.com>
2025-01-20 17:14:04 +0800

    review initialising two-dimensional array

---
+++ J-programmes/ArrayTest/ArrayTest001-Intialise/InitialiseTwoDimensionalArray.java
@@ -0,0 +1,8 @@
+
+public class InitialiseTwoDimensionalArray {
+
+    public static void main(String[] args) {
+        int[][] arr1 = new int[][2];  // The size of the first dimension of an array must be set when initialised.
+        int[][] arr = new int[2][];
+    }
+}
